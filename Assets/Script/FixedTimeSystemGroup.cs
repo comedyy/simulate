d5fa5f9f6b8fcc18887c28832678ec3f3422c477
张敦勇 <dunyong.zhang@igg.com>
2023-07-04 16:21:30 +0800

    追帧

--- Assets/Script/FixedTimeSystemGroup.cs
+++ Assets/Script/FixedTimeSystemGroup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Unity.Entities;
+using UnityEngine;
 
 public class FixedTimeSystemGroup : ComponentSystemGroup
 {
@@ -33,45 +34,77 @@ public class FixedTimeSystemGroup : ComponentSystemGroup
         var bufferHurt = EntityManager.GetBuffer<VHurtComponent>(GetSingletonEntity<VHurtComponent>());
         bufferHurt.Clear();
 
-        int count = UnityEngine.Random.Range(0, 5);
-
         var elapsedTime = UnityEngine.Time.time - _battleStartTime;
-        while (true)
+        var logicTime = GetSingleton<LogicTime>();
+        int needFrame = GetNeedCalFrame(_needRandomFixedCount, logicTime.frameCount, _localFrame.ReceivedServerFrame);
+        for(int i = 0; i < needFrame; i++)
         {
-            var logicTime = GetSingleton<LogicTime>();
-            var lastTime = logicTime.escaped;
-            if(logicTime.frameCount >= _localFrame.ReceivedServerFrame)
-            {
-                break;
-            }
+            // var lastTime = logicTime.escaped;
+            // if(logicTime.frameCount >= _localFrame.ReceivedServerFrame)
+            // {
+            //     break;
+            // }
 
             if(_flag.isEnd) return; // 游戏已经结束
 
-            var condition = true;
-            if(_needRandomFixedCount) condition = count-- > 0;
-            else condition = !_firstTickFinished || elapsedTime - lastTime >= logicTime.deltaTime;
+            // var condition = true;
+            // if(_needRandomFixedCount) 
+            // {
+            //     condition = count-- > 0;
+            // }
+            // else 
+            // {
+            //     condition = !_firstTickFinished || elapsedTime - lastTime >= logicTime.deltaTime;
+            // }
+            
 
-            if (condition)
+            // if (condition)
             // if((count--) > 0)
-            {
-                _firstTickFinished = true;
+            // {
+            //     _firstTickFinished = true;
                 
                 logicTime.escaped += logicTime.deltaTime;
                 logicTime.frameCount ++;
                 SetSingleton(logicTime);
 
                 World.SetTime(new Unity.Core.TimeData(float.NaN, float.NaN));   // 用来防止别人用到了Time对象，需要使用LogicTime， 更早发现问题
-
                 base.OnUpdate();  // Update
-            }
-            else
-            {
-                break;
-            }
+            // }
+            // else
+            // {
+            //     break;
+            // }
         }
     }
 
-    
+    private int GetNeedCalFrame(bool needRandomFixedCount, int frameCount, int receivedServerFrame)
+    {
+        if(needRandomFixedCount)
+        {
+            return UnityEngine.Random.Range(0, 5);
+        }
+
+        if(receivedServerFrame == int.MaxValue)
+        {
+            return 1;
+        }
+
+        if(frameCount < receivedServerFrame)
+        {
+            var offSet = receivedServerFrame - frameCount;
+            if(offSet < 50)
+            {
+                return Mathf.Min(2, offSet);
+            }
+            else
+            {
+                return Mathf.Min(4, offSet);
+            }
+        }
+
+        return 0;
+    }
+
     public override void SortSystemUpdateList()
     {
         var backup = new List<ComponentSystemBase>();
