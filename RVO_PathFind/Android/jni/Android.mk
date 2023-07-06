cabcfd29bcc5bdd465da8e5fe0981755a2f53987
张敦勇 <dunyong.zhang@igg.com>
2023-07-06 11:56:27 +0800

    安卓版本。

--- RVO_PathFind/Android/jni/Android.mk
+++ RVO_PathFind/Android/jni/Android.mk
@@ -5,8 +5,14 @@ LOCAL_MODULE := libProject
 LOCAL_LDLIBS     := -llog -landroid
 LOCAL_CFLAGS    := -DANDROID_NDK
 
-FILE_LIST := $(wildcard $(LOCAL_PATH)/../../RVO_PathFind/*.cpp, $(LOCAL_PATH)/../../libfixmath/*.cpp)
-LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+FILE_LIST1 := $(wildcard $(LOCAL_PATH)/../../libfixmath/*.cpp)
+FILE_LIST := $(wildcard $(LOCAL_PATH)/../../RVO_PathFind/*.cpp)
+
+$(warning $(FILE_LIST1))
+$(warning $(FILE_LIST))
+
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%) $(FILE_LIST1:$(LOCAL_PATH)/%=%)
+$(warning $(LOCAL_SRC_FILES))
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../RVO_PathFind/*.h,$(LOCAL_PATH)/../../libfixmath/*.h
 
